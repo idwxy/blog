d2533160d55492ceb48a1c8b10e5559dddf2f286
idwxy <idwxy@live.com>
2019-09-28 17:35:31 +0800

    添加 @Mapper 注解

--- src/main/java/com/idwxy/blog/BlogApplication.java
+++ src/main/java/com/idwxy/blog/BlogApplication.java
@@ -1,9 +1,12 @@
 package com.idwxy.blog;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// 添加 @MapperScan 注解，在 Spring Boot 启动的时候自动加载包路径下的 Mapper 接口
 @SpringBootApplication
+@MapperScan("com.idwxy.blog.dao")
 public class BlogApplication {
 
 	public static void main(String[] args) {
