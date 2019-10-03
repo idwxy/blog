bf124322e85c5c38cd245421e9f2f10397595668
idwxy <idwxy@live.com>
2019-10-03 22:37:31 +0800

    添加登录方法

--- src/main/java/com/idwxy/blog/dao/AdminUserMapper.java
+++ src/main/java/com/idwxy/blog/dao/AdminUserMapper.java
@@ -1,8 +1,17 @@
 package com.idwxy.blog.dao;
 
 import com.idwxy.blog.entity.AdminUser;
+import org.apache.ibatis.annotations.Param;
 
 public interface AdminUserMapper {
+
+    /**
+     * 登录方法
+     * @param userName
+     * @param password
+     * @return
+     */
+    AdminUser login(@Param("userName") String userName,@Param("password") String password);
     int deleteByPrimaryKey(Integer id);
 
     int insert(AdminUser record);
