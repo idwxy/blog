5303211c99d775ca8098047e5df547cf2cc4f19a
idwxy <idwxy@live.com>
2019-10-04 17:48:08 +0800

    建立管理员登录控制器

---
+++ src/main/java/com/idwxy/blog/controller/admin/AdminController.java
@@ -0,0 +1,60 @@
+package com.idwxy.blog.controller.admin;
+
+import com.idwxy.blog.Service.AdminUserService;
+import com.idwxy.blog.entity.AdminUser;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
+
+@RestController
+@RequestMapping("/admin")
+public class AdminController {
+
+    @Resource
+    private AdminUserService adminUserService;
+
+    // 跳转登录页面
+    @GetMapping("/login")
+    public String login() {
+        return "admin/login";
+    }
+
+    @PostMapping(value = "/login")
+    public String login(@RequestParam("userName") String userName,
+                        @RequestParam("password") String password,
+                        @RequestParam("verifyCode") String verifyCode,
+                        HttpSession session) {
+
+        if (StringUtils.isEmpty(verifyCode)) {
+            session.setAttribute("errorMsg", "验证码不能为空");
+            return "admin/login";
+        }
+        if (StringUtils.isEmpty(userName) || StringUtils.isEmpty(password)) {
+            session.setAttribute("errorMsg", "用户名或密码不能为空");
+            return "admin/login";
+        }
+        String kaptchaCode = session.getAttribute("verifyCode") + "";
+        if (StringUtils.isEmpty(kaptchaCode) || !verifyCode.equals(kaptchaCode)) {
+            session.setAttribute("errorMsg", "验证码错误");
+            return "admin/login";
+        }
+        AdminUser adminUser = adminUserService.login(userName, password);
+        if (adminUser != null) {
+            session.setAttribute("loginUser", adminUser.getUserName());
+            session.setAttribute("loginUserId", adminUser.getId());
+            // session 过期时间设置为 7200 秒 即两小时
+            session.setMaxInactiveInterval(60 * 60 * 2);
+            return "redirect:/admin/index";
+        } else {
+            session.setAttribute("errorMsg", "登陆失败");
+            return "admin/login";
+        }
+    }
+
+    @GetMapping({"", "/", "/index", "/index.html"})
+    public String index() {
+        return "admin/index";
+    }
+}
